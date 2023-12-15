2f0ac5b9ed5d1683cd83381cefbb0d08bb19b833
Soham-Mane <116940634+Soham-Mane@users.noreply.github.com>
2023-12-15 18:01:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,26 @@
 import React,{useState} from 'react';
-import Navbar from './components/Navbar';
-import {Box, ThemeProvider, createTheme} from '@mui/material';
-import Hero from './components/Hero';
-import About from './components/About';
-import Demo from './components/Demo';
-import Features from './components/Features';
-import Banner from './components/Banner';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes, Form } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import User from './components/User';
+import Chart from './components/Chart';
+import Modal from './components/Modal';
 import Contact from './components/Contact';
-import Footer from './components/Footer';
-import Testimonials from './components/Testimonials';
-import FAQ from './components/FAQ';
-import VideoPlayer from './components/VideoPlayer';
-import Testing from './components/Testing';
 function App() {
  
   return (
- < >
-  {/* <Navbar/> */}
-  <Banner/>
- <Hero/>
- <About/>
-<Features/>
-<Testimonials/>
-<Demo/>
-<Contact/>
-<FAQ/>
-<Footer/>
-{/* <VideoPlayer/> */}
+ <>
+<Router>
+  <Routes>
+    <Route exact path="/" element={<LandingPage/>}></Route>
+    <Route exact path="/user_demo" element={<User/>}></Route> 
+    <Route exact path="/chart" element={<Chart/>}></Route> 
+    <Route exact path="/modal" element={<Modal/>}></Route> 
+    <Route exact path="/contact" element={<Contact/>}></Route> 
+
+  </Routes>
+</Router>
+
 
  </>
   );
